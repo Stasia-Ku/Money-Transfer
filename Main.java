0c125cac06f945a698ff9204a56d3d7070bf60f1
Stasia-Ku <67242445+Stasia-Ku@users.noreply.github.com>
2020-06-29 14:31:12 +0300

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        int currient_ballance = 2_000_000_000;
+        int transition_value = 500_000_000;
+        int result_ballance = currient_ballance + transition_value;
+
+        System.out.println(result_ballance);
+    }
+}
